45e552e840b81d534a7dfa7f0db278b670217e24
Casole Giovanni <casolegiovanni@live.it>
2016-03-08 16:34:22 +0100

    C++

---
+++ BeginningCPPThroughGameProgramming/Chapter2/main.cpp
@@ -0,0 +1,277 @@
+// Score Rater
+// Demonstrates the if statements
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+int main()
+{
+	/*if (true)
+	{
+		std::cout << "This is always displayed.\n\n";
+	}
+
+	if (!false)
+	{
+		std::cout << "This always displayed either.\n\n";
+	}
+
+	if (false)
+	{
+		std::cout << "This is never displayed.\n\n";
+	}
+
+	if (!true)
+	{
+		std::cout << "This is never displayed either.\n\n";
+	}
+	
+	int score = 1000;
+
+	if (score)
+	{
+		std::cout << "At least you didn't score 0\n\n";
+	}
+
+	if (score >= 250)
+	{
+		std::cout << "You scored 250 or more. Decent.\n\n";
+	}
+
+	if (score >= 500)
+	{
+		std::cout << "You scored 500 or more. Nice.\n\n";
+		if (score >= 1000)
+		{
+			std::cout << "You scored 1000 or more. Impressive!\n";
+		}
+	}*/
+
+	/*int score;
+	std::cout << "Enter your score: ";
+	std::cin >> score;
+
+	if (score >= 1000)
+	{
+		std::cout << "You scored 1000 or more. Impressive!\n";
+	}
+	else if (score >= 500)
+	{
+		std::cout << "You scored 500 or more. Nice.\n";
+	}
+	else if (score >= 250)
+	{
+		std::cout << "You scored 250 or more. Decent.\n";
+	}
+	else
+	{
+		std::cout << "You scored less than 250. Nothing to brag about.\n";
+	}*/
+
+	/*enum Difficulties : int
+	{
+		Easy = 1, Normal, Hard
+	};
+
+	std::cout << "Difficulty Levels\n\n";
+	std::cout << "1 - Easy\n";
+	std::cout << "2 - Normal\n";
+	std::cout << "3 - Hard\n\n";
+
+	int choise;
+	std::cout << "Choise: ";
+	std::cin >> choise;
+
+	Difficulties difficulty = static_cast<Difficulties>(choise);
+
+	switch (difficulty)
+	{
+	case Easy:
+		std::cout << "You picked Easy. \n";
+		break;
+	case Normal:
+		std::cout << "You picked Normal. \n";
+		break;
+	case Hard:
+		std::cout << "You picked Hard. \n";
+		break;
+	default:
+		std::cout << "You made an illegal choise";
+		break;
+	}*/
+
+	/*char again = NULL;
+	while (again == 'y')
+	{
+		std::cout << "\n**Played an exiting game**";
+		std::cout << "\nDo you want to play again? (y/n): ";
+		std::cin >> again;
+	}
+	std::cout << "\nOkay, bye.";*/
+
+	/*char again = NULL;
+	do
+	{
+		std::cout << "\n**Played an exiting game**";
+		std::cout << "\nDo you want to play again? (y/n): ";
+		std::cin >> again;
+	} while (again == 'y');
+
+	std::cout << "\nOkay, bye.";*/
+
+	/*int count = 0;
+	while (true)
+	{
+		count += 1;
+		// end loop if count is greater than 10
+		if (count > 10)
+		{
+			break;
+		}
+
+		// skip the number 5
+		if (count == 5)
+		{
+			continue;
+		}
+
+		std::cout << count << std::endl;
+	}*/
+
+	/*std::string username;
+	std::string password;
+	bool success;
+
+	std::cout << "\tGame Designer's Network\n";
+	do
+	{
+		std::cout << "\nUsername: ";
+		std::cin >> username;
+
+		std::cout << "\nPassword: ";
+		std::cin >> password;
+
+		if (username == "S.Meier" && password == "civilization")
+		{
+			std::cout << "\nHey, Sid.";
+			success = true;
+		}
+		else if (username == "S.Miyamoto" && password == "mariobros")
+		{
+			std::cout << "\nWhat's up, Shigeru?";
+			success = true;
+		}
+		else if (username == "W.Wright" && password == "thesims")
+		{
+			std::cout << "\nHow goes it, Will?";
+			success = true;
+		}
+		else if (username == "guest" || password == "guest")
+		{
+			std::cout << "\nWelcome, guest.";
+			success = true;
+		}
+		else
+		{
+			std::cout << "\nYour login failed.";
+			success = false;
+		}
+	} while (!success);*/
+
+	/*srand(static_cast<unsigned int>(time(0))); // seed randmon number generator
+	int randomNumber = rand();
+
+	int die = (randomNumber % 6) + 1; // get a number between 1 and 6
+
+	std::cout << "You rolled a " << die << std::endl;*/
+
+	/*srand(static_cast<unsigned int>(time(0))); // seed the random generator
+
+	int numberToGuess = (rand() % 100) + 1; // generate a random number
+	int numberGuessed;
+	int numberOfTries = 0;
+
+	std::cout << "Ho generato un numero da 1 a 100. Indovinalo! ";
+	do
+	{
+		std::cin >> numberGuessed;
+		++numberOfTries;
+
+		if (numberGuessed != numberToGuess)
+		{
+			if (numberGuessed > numberToGuess)
+			{
+				std::cout << "Troppo alto!" << std::endl;
+				std::cout << "Riprova! ";
+			}
+			else
+			{
+				std::cout << "Troppo basso!" << std::endl;
+				std::cout << "Riprova! ";
+			}
+		}
+		else
+		{
+			std::cout << "Complimenti! Hai indovinato!" << std::endl;
+			std::cout << "Hai indovinato dopo " << numberOfTries << " tentativi." << std::endl;
+		}
+	} while (numberGuessed != numberToGuess);*/
+
+	/* Questo è un semplicissimo gioco in cui il giocatore 
+		pensa a un numero e il computer lo deve indovinare
+		in base alle indicazioni "Troppo alto" o "Troppo basso"
+		
+		Programmatore: Casole Giovanni
+		Data: 08/03/2016
+	*/
+
+	enum Answer : int
+	{
+		Yes = 1, TooHigh, TooLow
+	};
+
+	srand(static_cast<unsigned int>(time(0))); // seed del generatore di numeri random
+	
+	int numberOfTries = 0;
+	int numberGuessed;
+	int max = 101;
+	int min = 1;
+	int response;
+	Answer userAnswer;
+	
+	std::cout << "Ciao pensa a un numero da 1 a 100 ed io lo indovinero'!" << std::endl;
+	do
+	{
+		numberGuessed = rand() % (max - min) + min; // genera numero random nel range min - max
+		++numberOfTries;
+		
+		std::cout << "Credo sia " << numberGuessed << ". Ho indovinato? " << std::endl << std::endl;
+		std::cout << "1. Si, hai indovinato!" << std::endl << std::endl;
+		std::cout << "2. No, troppo alto!" << std::endl << std::endl;
+		std::cout << "3. No, troppo basso!" << std::endl << std::endl;
+
+		std::cin >> response;
+		userAnswer = static_cast<Answer>(response); // converte la variabile di tipo int in Answer
+
+		switch (userAnswer)
+		{
+		case Yes:
+			std::cout << "Ho indovinato in " << numberOfTries << " tentativi." << std::endl;
+			break;
+		case TooHigh:
+			max = numberGuessed;
+			break;
+		case TooLow:
+			min = numberGuessed + 1;
+			break;
+		default:
+			std::cout << "Non ho capito!" << std::endl;
+			break;
+		}
+		
+	} while (userAnswer != Yes);
+
+	return 0;
+}
--- CPPprimer/test/main.cpp
+++ CPPprimer/test/main.cpp
@@ -1,50 +1,200 @@
-/*
-*	A simple program that prompt the user to insert two variables and give the pruduct
-*
-*	Author: Gianni
-*	Created: 03/01/2016
-*/
-
 #include <iostream>
 #include "Sales_item.h"
-
-using std::cout;
-using std::cin;
-using std::endl;
-
-int getFactorial(int number) {
-	int sum;
-
-	if (number == 1) sum = 1;
-	else sum = getFactorial(number - 1) * number;
-
-	return sum;
-}
-
-void add(int* value) {
-	*value += 2;
-}
-
+/*
+* Simple main function:
+* Read two numbers and write their sum
+*/
 int main()
 {
-	int largestNumber = 2147483647;
+	//// Proompt the user to enter two numbers
+	//std::cout << "Inserisci due numeri: ";
+	//int v1 = 0, v2 = 0; // variables to hold the input we read
+	//std::cin >> v1 >> v2; // read input
+	//std::cout << "La somma dei due numeri e': ";
+	//std::cout << v1 * v2;
+	//std::cout << std::endl;
 
-	int num = 5;
+	//int sum = 0, val = 50;
 
-	cout << "1: " << getFactorial(1) << endl;
-	cout << "2: " << getFactorial(3) << endl;
+	//while (val <= 100) // while val is less than or equal to 100
+	//{
+	//	sum += val;
+	//	++val;
+	//}
 
-	add(&num);
+	//std::cout << "La somma dei numeri da 50 a 100 e': " << sum << std::endl;
 
-	cout << num << endl;
+	//std::cout << "Stampa dei numeri da 10 a 0: ";
+	//int num = 10;
+	//while (num >= 0) // while num is greater than or equal to 0
+	//{
+	//	std::cout << num << " ";
+	//	--num;
+	//}
 
-	cout << "Largest Number: " << largestNumber << endl;
+	//std::cout << std::endl << "Inserisci due numeri: ";
+	////int v1 = 0, v2 = 0;
+	//std::cin >> v1 >> v2;
 
-	Sales_item book, book2;
+	//if (v1 < v2) // checks if v1 is less than v2
+	//{
+	//	while (v1 <= v2) // while v1 is less than or equal to v2
+	//	{
+	//		std::cout << v1 << std::endl;
+	//		++v1;
+	//	}
+	//}
+	//else 
+	//{
+	//	while (v1 >= v2) // while v1 is greater than or equal to v2
+	//	{
+	//		std::cout << v1 << std::endl;
+	//		--v1;
+	//	}
+	//}
+	//
+	//sum = 0;
+	//for (int i = -100; i <= 100; ++i)
+	//{
+	//	sum += i;
+	//}
 
-	cin >> book >> book2;
+	//sum = 0;
+	//for (int i = 50; i <= 100; ++i)
+	//{
+	//	sum += i;
+	//}
 
-	cout << book + book2 << endl;
+	//std::cout << "Stampa dei numeri da 10 a 0: ";
+
+	//for (int i = 10; i >= 0; --i)
+	//{
+	//	std::cout << i << " ";
+	//}
+
+	/*std::cout << "Inserisci due interi: ";
+	int small = 0, big = 0;
+	std::cin >> small >> big;
+
+	if (small > big)
+	{
+		int tmp = small;
+		small = big;
+		big = tmp;
+	}
+
+	for (int i = small; i <= big; ++i)
+	{
+		std::cout << i << " ";
+	}*/
+
+	/*std::cout << "Inserisci interi: ";
+	int sum = 0, val = 0;
+
+	while (std::cin >> val)
+	{
+		sum += val;
+	}
+	
+	std::cout << std::endl << "La somma dei numeri inseriti e': " << sum;*/
+
+	/*std::cout << "Inserisci alcuni interi: ";
+
+	int valCor = 0;
+	int val = 0;
+
+	if (std::cin >> valCor)
+	{
+		int conteggio = 1;
+		while (std::cin >> val)
+		{
+			if (val == valCor)
+			{
+				++conteggio;
+			}
+			else
+			{
+				std::cout << valCor << " inserito " << conteggio << " volte. " << std::endl;
+				conteggio = 1;
+				valCor = val;
+			}
+		}
+		std::cout << valCor << " inserito " << conteggio << " volte. " << std::endl;
+	}
+	else
+	{
+		std::cout << "Non hai inserito alcun numero" << std::endl;
+	}*/
+
+	/*Sales_item book;
+
+	while (std::cin >> book)
+	{
+		std::cout << book << std::endl;
+	}*/
+
+	/*Sales_item book1, book2;
+
+	std::cin >> book1 >> book2;
+
+	std::cout << book1 + book2;*/
+
+//Sales_item sumBook;
+//if (std::cin >> sumBook)
+//{
+//	Sales_item trans;
+//	while (std::cin >> trans)
+//	{
+//		if (sumBook.isbn() == trans.isbn())
+//		{
+//			sumBook += trans;
+//		}
+//		else
+//		{
+//			std::cout << "Codice ISBN diverso" << std::endl;
+//			std::cout << sumBook << std::endl;
+//			std::cout << trans << std::endl;
+//		}
+//	}
+//	std::cout << sumBook << std::endl;
+//}
+//else
+//{
+//	std::cerr << "ERROR: NO DATA!" << std::endl;
+//	return -1;
+//}
+
+Sales_item curBook;
+Sales_item book;
+
+std::cout << "Inserisci le transazioni: " << std::endl;
+
+if (std::cin >> curBook)
+{
+ 	int conteggio = 1;
+	while (std::cin >> book)
+	{
+		if (book.isbn() == curBook.isbn())
+		{
+			++conteggio;
+			curBook += book;
+		}
+		else
+		{
+			//std::cout << curBook << std::endl;
+			std::cout <<  curBook << " e' stato venduto " << conteggio << " volte. " << std::endl;
+			conteggio = 1;
+			curBook = book;
+		}
+	}
+	//std::cout << curBook << std::endl;
+	std::cout << curBook << " e' stato venduto " << conteggio << " volte. " << std::endl;
+}
+else
+{
+	std::cerr << "ERRORE: NESSUN DATO!" << std::endl;
+	return -1;
+}
 
 	return 0;
 }
